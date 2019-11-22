b94df10d420c7fc3d8ce7f0137575ce76b4233b3
Ahmed Nabih <ahmed.nabih01@gmail.com>
2019-11-23 00:54:33 +0200

    testing circleci

--- src/test/java/MathTopics/TestCircleCircumference.java
+++ src/test/java/MathTopics/TestCircleCircumference.java
@@ -4,12 +4,12 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TestCircleCircumference {
-	
+
 	@Test
 	public void CircleCircumferenceTest() {
 		double TestRes = CircleCircumference.GetCircleCircumference(5);
 
-		double ans = 440 / 7 + 1;
+		double ans = 440 / 7;
 
 		assertEquals(ans, TestRes, 0);
 	}
