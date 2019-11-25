ce1c3aee5844cdffe665542a5fb08c404aa044a6
Mohammad Ashraf <g.mohammad.ashraf.99@gmail.com>
2019-11-25 04:49:12 +0200

    to pull request

--- src/main/java/MathTopics/SphereArea.java
+++ src/main/java/MathTopics/SphereArea.java
@@ -1,32 +1,20 @@
 package MathTopics;
 
 import git_assignment_initial.ITopic;
-<<<<<<< HEAD
 import git_assignment_initial.PrintStyle;
-=======
->>>>>>> origin/master
 
 //Please read CicleArea.java Code and implement as a clone of it but using your formula
 
 public class SphereArea implements ITopic {
 	public static double GetShpereArea(int r) {
-<<<<<<< HEAD
 		return (double) 4 * 22/7 * Math.pow(r, 2);
-=======
-		return (double)8800 / 7;
->>>>>>> origin/master
 	}
 
 	@Override
 	public String NotifyTopic(int input) {
-<<<<<<< HEAD
 		double ans = GetShpereArea(input);
 		String output = PrintStyle.print(input, "Fib") + ans;
 		return output;
-=======
-
-		return "Input = 10\nShpere Area = " + (double)8800 / 7;
->>>>>>> origin/master
 	}
 
 }
